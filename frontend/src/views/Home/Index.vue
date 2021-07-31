a1022b26603cf4ee022c7d2ea581be4c6a01e4b0
jmayuh6 <62195740+jmayuh6@users.noreply.github.com>
2021-07-31 14:55:58 -0500

    Add files via upload

---
+++ frontend/src/views/Home/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home text-light">
+    <BaseHeaderTitle
+      title="Detección de Parkinson mediante algoritmos de Inteligencia Artificial, utilizando audios"
+      subtitle1="Mayuri Hidalgo, Jesus"
+      subtitle2="Medina Apaico, Walter"
+    >
+    </BaseHeaderTitle>
+
+    <div class="mb-5" align="center">
+      <img src="../../assets/logouni.png" alt="Logo UNI" class="w-15">
+    </div>
+
+    <div class="text-monospace">
+      <p class="lead" align="justify">
+        La enfermedad del Parkinson es una enfermedad progresiva y degenerativa que afecta al
+sistema nervioso, los síntomas son conocidos recién
+después de 4 años del inicio de la enfermedad, por lo que
+existe la necesidad de aplicar ciertos métodos para
+realizar un diagnóstico temprano de esta enfermedad, y
+uno de ellos está relacionado con la voz. Para ello, se
+aplicarán diversas técnicas actuales de Machine
+Learning con el objetivo de encontrar el algoritmo
+adecuado que permita la detección de Parkinson en
+estados tempranos.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseHeaderTitle from '@/components/BaseHeaderTitle'
+
+export default {
+  name: 'home',
+  components: {
+    BaseHeaderTitle
+  }
+}
+</script>
