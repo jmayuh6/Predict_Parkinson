46c9dbf09c1582808c9b2651f266fbdfe90d4eb8
jmayuh6 <62195740+jmayuh6@users.noreply.github.com>
2021-07-31 14:50:48 -0500

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+
+    <BaseNavigationBar/>
+
+    <BaseLayout>
+
+      <router-view/>
+
+    </BaseLayout>
+
+  </div>
+</template>
+
+<script>
+import BaseNavigationBar from './components/BaseNavigationBar'
+import BaseLayout from './components/BaseLayout'
+
+export default {
+  components: { BaseNavigationBar, BaseLayout }
+}
+</script>
+
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    color #808080
+    background-color #1A1E24
+    background-color #2f3a4a
+</style>
